6eca1baede8ed876ffd434966b435cd272a2b17d
Ole Martin Gjersvik <olem@olem.org>
2019-06-25 19:15:20 +0200

    Added mega simple parser.

--- src/lib.rs
+++ src/lib.rs
@@ -24,30 +24,48 @@ struct Token<'a> {
     slice: &'a str,
 }
 
-//enum Exp {
-//    Print(Box<Exp>),
-//    LiteralUInt(u64)
-//}
+#[derive(Debug)]
+enum Exp {
+    Print(Box<Exp>),
+    LiteralUInt(u64),
+    InValid,
+}
 
 //enum Value {
 //    Nothing,
 //    UInt(u64)
 //}
 
-fn tokenize(code: &str) -> Vec<Token>{
+fn tokenize(code: &str) -> Vec<Token> {
     let mut tokens = Vec::new();
     let mut lexer = TokenType::lexer(code);
     while lexer.token != TokenType::End {
-        tokens.push(Token { token_type: lexer.token, slice: lexer.slice()});
+        tokens.push(Token {
+            token_type: lexer.token,
+            slice: lexer.slice(),
+        });
         lexer.advance();
     }
     return tokens;
 }
 
+fn parse(tokens: &[Token]) -> Exp {
+    let token = &tokens[0];
+    let rest = &tokens[1..];
+    match token.token_type {
+        TokenType::Print => return Exp::Print(Box::new(parse(rest))),
+        TokenType::Number => return Exp::LiteralUInt(token.slice.parse().unwrap()),
+        _ => return Exp::InValid,
+    }
+}
+
 pub fn run(code: &str) {
     let tokens = tokenize(code);
-    for token in tokens {
-        println!("{:?}: {}", token.token_type, token.slice)
+    for token in &tokens {
+        println!("Token {:?}: {}", token.token_type, token.slice)
     }
+    let exp = parse(&tokens);
+    println!("Exp: {:?}", exp);
+
     println!("Program done");
 }

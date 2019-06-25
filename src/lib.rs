1a9509f63962300b525a7c6f518e2d7535b1c713
Ole Martin Gjersvik <olem@olem.org>
2019-06-25 18:45:08 +0200

    Moved tokenize into its own function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 use logos::Logos;
+use std::vec::Vec;
 
-#[derive(Logos, Debug, PartialEq)]
-enum Token {
+#[derive(Logos, Debug, Clone, Copy, PartialEq)]
+enum TokenType {
     #[end]
     End,
 
@@ -18,11 +19,35 @@ enum Token {
     EndOfExp,
 }
 
-pub fn run(code: &str) {
-    let mut lexer = Token::lexer(code);
-    while lexer.token != Token::End {
-        println!("{:?}: {}", lexer.token, lexer.slice());
+struct Token<'a> {
+    token_type: TokenType,
+    slice: &'a str,
+}
+
+//enum Exp {
+//    Print(Box<Exp>),
+//    LiteralUInt(u64)
+//}
+
+//enum Value {
+//    Nothing,
+//    UInt(u64)
+//}
+
+fn tokenize(code: &str) -> Vec<Token>{
+    let mut tokens = Vec::new();
+    let mut lexer = TokenType::lexer(code);
+    while lexer.token != TokenType::End {
+        tokens.push(Token { token_type: lexer.token, slice: lexer.slice()});
         lexer.advance();
     }
-    println!("Lexing done");
+    return tokens;
+}
+
+pub fn run(code: &str) {
+    let tokens = tokenize(code);
+    for token in tokens {
+        println!("{:?}: {}", token.token_type, token.slice)
+    }
+    println!("Program done");
 }

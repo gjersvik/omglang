faae674d5e0e256b47ded3d700b15b1a61d74f51
Ole Martin Gjersvik <olem@olem.org>
2019-06-26 08:04:19 +0200

    Load code from file.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+use clap::{App, Arg};
+use std::fs;
+
 use omglang::run;
 
 fn main() {
-    run("print 42;");
+    let matches = App::new("O.M.G. Language")
+        .version("0.0.0")
+        .author("Ole Martin Gjersvik")
+        .about("The multi core language.")
+        .arg(
+            Arg::with_name("SRC_FILE")
+                .help("the .omg file to run")
+                .required(true)
+                .index(1),
+        )
+        .get_matches();
+
+    let source =
+        fs::read_to_string(matches.value_of("SRC_FILE").unwrap()).expect("could not read file");
+    run(&source);
 }
